7f6bbf0683bf97851760ac4b60a6004dc4035f31
Johan Melin <soldierheart@hotmail.com>
2021-09-08 14:42:56 +0200

    card image

--- src/components/CardArea.js
+++ src/components/CardArea.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 const axios = require('axios');
 
 const CardArea = () => {
+    const [score, setScore] = useState(0);
     const [cards, setCards] = useState({});
+    const [cardImage, setCardImage] = useState(null);
 
     function getCards(){
-        console.log("getCards")
         const cardUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
         axios.get(cardUrl)
       .then(function (response) {
@@ -14,15 +15,55 @@ const CardArea = () => {
       })
       .catch(function (error) {
         console.log(error);
+      }).then(drawCard());
+    }
+
+    const drawCard = () => {
+        const cardUrl = `https://deckofcardsapi.com/api/deck/${cards.deck_id}/draw/?count=1`;
+        axios.get(cardUrl)
+      .then(function (response) {
+        setCards(response.data);
+        setCardImage(response.data.cards[0].image);
+
+      })
+      .catch(function (error) {
+        console.log(error);
       });
     }
+
+    const guessHigher = () => {
+        drawCard();
+    }
     
+    const guessLower = () => {
+        drawCard();
+    }
+
+    const renderImage = () => {
+        const imgSource=cardImage;
+        return (
+            <Image
+                style={{width: 200, height: 200}}
+                source={{uri: imgSource}}
+            />
+        )
+    }
+
     useEffect(() => {
         getCards();
     }, []);
-        return (
+
+    return (
         <View>
+            <Text>Score: {score}</Text>
             <Text>Cards remaining: {cards.remaining}</Text>
+            {renderImage()}
+            <TouchableOpacity onPress={() => guessHigher()}>
+                <Text>Higher</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => guessLower()}>
+                <Text>Lower</Text>
+            </TouchableOpacity>
         </View>
     )
 }
